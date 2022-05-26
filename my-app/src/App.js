14c38d3d854a643e2915884b4a120d5702558dc6
gift56 <efeasiughu@gmail.com>
2022-05-26 12:42:18 +0100

    handling responsiveness

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,7 @@ import Navbar from './component/navbar/Navbar';
 import Banner from './component/navbar/Banner';
 
 function App() {
+  document.title = "Navbar";
   return (
     <div className="App">
       <Navbar />
