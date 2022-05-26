2bb41975bc1cda55163a340db65b26a8b7dd034b
gift56 <efeasiughu@gmail.com>
2022-05-26 13:25:06 +0100

    hangled responsiveness issues

--- my-app/src/component/navbar/Navbar.jsx
+++ my-app/src/component/navbar/Navbar.jsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 import style from './css/navbar.module.css';
 
 const Navbar = () => {
+    const [isMobile, setIsMobile] = useState(false);
+
+    const handleMobileMenu = () => {
+        setIsMobile((prev) => !prev);
+    };
+
     return (
         <nav className={style.nav}>
             <div className={style.logo}>
                 <h1>My Logo</h1>
             </div>
-            <div className={style.links}>
+            <span className={style.ham} onClick={handleMobileMenu}>
+                {isMobile ? <FaTimes /> : <FaBars />}
+            </span>
+            <div className={` ${isMobile ? style.show : style.links}`}>
                 <a href="#Home">Home</a>
                 <a href="#About">About</a>
                 <a href="#Service">Service</a>
