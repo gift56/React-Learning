7d8d2300384b467a5abe1f331a8c06db47dc7566
gift56 <efeasiughu@gmail.com>
2022-05-26 12:07:48 +0100

    working on the project

--- my-app/src/component/navbar/Banner.jsx
+++ my-app/src/component/navbar/Banner.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from 'react';
+import bannerStyle from './css/Banner.module.css';
+import codingImg from './img/codingbreast.png';
 
 const Banner = () => {
-  return (
-    <div>
-      <h1>My Banner</h1>
-    </div>
-  )
+    return (
+        <section  className={bannerStyle.bannerSec}>
+            <div className={bannerStyle.bannerText}>
+                <h2>I love coding</h2>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus quod, qui, assumenda quae voluptatum inventore est totam amet dolor provident accusantium necessitatibus veniam sit distinctio, cumque quasi neque aperiam porro ea. Incidunt repudiandae nulla explicabo odit atque quibusdam eveniet qui aliquam, quis et vero voluptates odio eaque recusandae quae natus.
+                </p>
+            </div>
+            <div className={bannerStyle.imgsec}>
+                <img src={codingImg} alt="coding" />
+            </div>
+        </section>
+    )
 }
 
 export default Banner;
