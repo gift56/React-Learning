89a29375a025e42cabdc453036ed21cf4383d461
gift56 <efeasiughu@gmail.com>
2022-07-28 10:18:16 +0100

    writing some logic

--- my-app/src/component/darktheme/Darkmood.jsx
+++ my-app/src/component/darktheme/Darkmood.jsx
@@ -4,12 +4,51 @@ import { HiMoon } from 'react-icons/hi';
 import styled from 'styled-components';
 
 
-const Darkmood = () => {
+const Darkmood = ({ theme, setTheme }) => {
+    const changeTheme = () => {
+        if (theme === 'light') {
+            setTheme('dark')
+        }
+        else {
+            setTheme('light')
+        }
+    }
+    const icon = theme === 'light' ? <HiMoon size={40} /> : <CgSun size={40} />
     return (
-        <div>
-            Dark area
-        </div>
+        <Page>
+            <Container>
+                <Toggle onClick={changeTheme}>
+                    {icon}
+                </Toggle>
+                <Title>Coding for today</Title>
+                <TagLine>Level Up your prestigsh</TagLine>
+            </Container>
+        </Page>
     )
 }
 
-export default Darkmood
+export default Darkmood;
+
+const Page = styled.div`
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      min-height: 100vh;
+      width: 100%;
+      background-color: ${theme => theme.pageBackground};
+      transition: 300ms ease;
+`
+
+const Container = styled.div`
+     display: flex; 
+     align-items: center;
+     flex-direction: column;
+`
+
+const Title = styled.h1`
+     color: ${theme => theme.titleColor};
+     transition: 300ms ease;
+     
+`
+const TagLine = styled.span``
+const Toggle = styled.span``
